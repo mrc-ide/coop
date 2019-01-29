498367b6dd4c84cba9d12813abdf4ac84aaa8b1b
Peter Winskill <p.winskill@imperial.ac.uk>
2019-01-29 16:23:16 +0000

    fix testing

--- tests/testthat/test-Min Max.R
+++ tests/testthat/test-Min Max.R
@@ -1,9 +1,9 @@
 context("Min Max")
 
 test_that("Min_max", {
-  dc <- c(1, 1, 2, 3, 3, 3, 10, 10, 10, 11, 11, 5, 5)
+  dc <- data.frame(ISO = "A", DIDE_CODE = c(1, 1, 2, 3, 3, 3, 10, 10, 10, 11, 11, 5, 5))
   ms <- mins(dc)
-  ma <- maxs(ms, length(dc))
+  ma <- maxs(ms, nrow(dc))
   expect_true(all(ma >= ms))
   expect_true(all(diff(ms) > 0))
   expect_true(all(diff(ma) > 0))
--- tests/testthat/test-Simple SA.R
+++ tests/testthat/test-Simple SA.R
@@ -1,10 +1,11 @@
 context("Simple SA")
 
 test_that("Small data SA", {
-  sa1 <- sa(small_data, small_data_budget, outer = 1000, inner = 10)
-  expect_true(all(small_data[sa1[[1]],"y"] == 2))
+  sa1 <- sa(small_data, small_data_budget, outer = 1000, inner = 10, frozen = 100)
+  expect_true(all(sa1$solution$y == 2))
 
-  sa2 <- sa(small_data, small_data_budget, outer = 1000, inner = 10, free = 1000)
-  expect_equal(sum(small_data[sa2[[1]],"y"] == 2), 15)
-  expect_equal(sum(small_data[sa2[[1]],"y"] == 1), 1)
+  sa2 <- sa(small_data, small_data_budget, outer = 1000, inner = 10,
+            free = 1000, frozen = 100)
+  expect_equal(sum(sa2$solution$y == 2), 15)
+  expect_equal(sum(sa2$solution$y == 1), 1)
 })
